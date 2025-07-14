453bf99fefc82f87a55a69d50ae1d3de3ed6e51a
andrewzhang451 <zhangandrew2020@gmail.com>
2025-07-14 14:30:45 -0500

    added some nav stuff

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,21 @@
+function Navbar() {
+    return (
+        <nav className="bg-blue-100 p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="text-2xl font-bold text-gray-800">11th Ward</div>
+                <div>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Home</a>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">About</a>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
+                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
+                    
+                </div>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import CommunityHighlights from "../components/CommunityHighlights";
 import QuickActions from "../components/QuickActions";
+import Navbar from "../components/navbar/navbar";
 
 function LandingPage() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-white to-blue-50">
+      <Navbar />
       <Header />
       <CommunityHighlights />
       <QuickActions />
