09a909ed6072ae6421d84f4e162b0d8ca6ccb266
andrewzhang451 <zhangandrew2020@gmail.com>
2025-07-15 14:37:44 -0500

    added css to navbar, as well as made it horizontal + dropdown functionality

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,19 +1,63 @@
 function Navbar() {
     return (
-        <nav className="bg-blue-100 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="text-2xl font-bold text-gray-800">11th Ward</div>
-                <div>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Home</a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">About</a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
-                    <a href="#" className="text-gray-700 hover:text-gray-900 mx-2">Contact</a>
-                    
-                </div>
+        <nav className="flex justify-evenly items-center gap-4">
+            <div>
+                <a href="#">Home</a>
             </div>
-
+            <div>
+                <a href="#">Service</a>
+            </div>
+            <div>
+                <ul className="flex gap-4">
+                    <li className="relative group">
+                        <a href="#" className="hover:underline">Our Mission</a>
+                            <ul className="absolute hidden group-hover:block bg-white shadow-md p-2 top-full">
+                                <li>
+                                    <a href="#" className="block px-4 py-2 hover:bg-gray-100">endorsement</a>
+                                </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex gap-4">
+                    <li className="relative group">
+                        <a href="#" className="hover:underline">About</a>
+                        <ul className="absolute hidden group-hover:block bg-white shadow-md p-2 top-full">
+                            <li>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100">Nicole Lee</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100">staff</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100">Our Accomplishment</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul className="flex gap-4">
+                    <li className="relative group">
+                        <a href="#" className="hover:underline">Connect</a>
+                        <ul className="absolute hidden group-hover:block bg-white shadow-md p-2 top-full">
+                            <li>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100">Email Nicole</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 hover:bg-gray-100">Resources</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <a href="#">Ward Map</a>
+            </div>
+            <div>
+                <a href="#">Gallery</a>
+            </div>
         </nav>
     );
 }
