917422884bf1f73bb2cbd42cd0d859166192d938
andrewzhang451 <zhangandrew2020@gmail.com>
2025-07-24 11:48:21 -0500

    added carousel functionality

---
+++ src/components/carousel.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+
+function Carousel() {
+
+  const data = [1, 2, 3, 4]
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  //create function to to do the rotation
+
+  const carouselInfiniteScroll = () => {
+    if (currentIndex === data.length - 1) {
+      return setCurrentIndex(0)
+    }
+    return setCurrentIndex(currentIndex + 1)
+  };
+
+
+  //useEffect () =>  
+
+  useEffect(() => {
+    const interval = setInterval(() => { carouselInfiniteScroll() }, 3000)
+
+    return () => clearInterval(interval)
+  })
+
+  return (
+    <div className="carousel-outer overflow-hidden w-full">
+      <div
+        className="carousel-inner flex transition-all duration-1000 ease-[cubic-bezier(0.31,0.575,0.565,1)]"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {data.map((item, index) => (
+          <h1
+            className="carousel-item h-[20rem] min-w-full w-full flex items-center justify-center"
+            key={index}
+          >
+            Newsletter {item}
+          </h1>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import CommunityHighlights from "../components/CommunityHighlights";
 import QuickActions from "../components/QuickActions";
 import Navbar from "../components/navbar/navbar";
 import Header_image from "../components/header_pic";
+import Carousel from "../components/carousel";
 
 function Home_Page() {
   return (
@@ -10,6 +11,7 @@ function Home_Page() {
       <Navbar />
       <Header_image />
       <Header />
+      <Carousel />
       <CommunityHighlights />
       <QuickActions />
       
